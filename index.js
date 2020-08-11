73fdaf312428539cdd655ec07d46485eaad30c21
Malik DeJean <mdejean@MacBook-Pro.local>
2020-08-10 22:29:28 -0700

    more jquery

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ function main() {
     e.preventdefault();
     let item = $('#shopping-list-entry').val();
     console.log(item);
+    $('.shopping-list').append(
+      `<li>
+        <span class="shopping-item">${item}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
   });
 
 }
